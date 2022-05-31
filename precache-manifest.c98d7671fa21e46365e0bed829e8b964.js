bcefadf563a83abf527b9c1dff4a6e44af7af8af
AbdallaM-Ibrahim <Abdalla.mostafa19200@gmail.com>
2022-05-31 05:40:05 +0200

    Updates

---
+++ precache-manifest.c98d7671fa21e46365e0bed829e8b964.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "b14130c0c803d5f04d48",
+    "url": "/robofriends/static/css/main.2b04b06f.chunk.css"
+  },
+  {
+    "revision": "b14130c0c803d5f04d48",
+    "url": "/robofriends/static/js/main.b14130c0.chunk.js"
+  },
+  {
+    "revision": "7f8c91ddc4b58ecb5b87",
+    "url": "/robofriends/static/js/1.7f8c91dd.chunk.js"
+  },
+  {
+    "revision": "1f278e67652f4fb19fc3",
+    "url": "/robofriends/static/css/2.6dcb3d61.chunk.css"
+  },
+  {
+    "revision": "1f278e67652f4fb19fc3",
+    "url": "/robofriends/static/js/2.1f278e67.chunk.js"
+  },
+  {
+    "revision": "a433ede44710983729ce",
+    "url": "/robofriends/static/js/runtime~main.a433ede4.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "c7aca7c5297f0d3cadd49d55b61bf577",
+    "url": "/robofriends/index.html"
+  }
+];
